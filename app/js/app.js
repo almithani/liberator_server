faf551e2c6bede9632937a30a92c2cec89b3374d
Al Mithani <almithani@gmail.com>
2018-08-31 19:41:42 -0400

    refining the js and adding a bit of UI to the book view.  also added scroll handler to decide when to load the next page

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,54 @@
 'use strict';
 
+
+/*
+	The purpose of this class is to abstract the "book" UI
+*/
+class liberator_book_app {
+
+	constructor(_targetElementId){ 
+		this.bookEl = document.getElementById("book");
+	}
+
+	init() {
+		const lib = new liberator_client();
+		
+		lib.getBookStylesheet().then( (styles) => {
+			var styleEl = document.createElement('style');
+			styleEl.innerHTML = styles;
+			this.bookEl.appendChild(styleEl);
+		});
+
+		lib.getBookContent().then( (content) => {
+			var bookContentEl = document.createElement('div');
+			bookContentEl.innerHTML = content
+			this.bookEl.appendChild(bookContentEl);
+		});	
+
+		this.bookEl.onscroll = this.scrollEventHandler;
+	}
+
+	scrollEventHandler(e) {
+		var viewableHeight = bookEl.clientHeight;
+		var contentHeight = bookEl.scrollHeight;
+		var totalScrolled = bookEl.scrollTop;
+
+		var heightLeft = contentHeight - totalScrolled - viewableHeight;
+		
+		//this is a percent that we use to decide to load the next 'page'
+		var NEXT_PAGE_LOAD_THRESHOLD = 1000; 
+
+
+		if ( heightLeft <= NEXT_PAGE_LOAD_THRESHOLD ) {
+			console.log('load next page');
+		}
+	}
+}
+
+
+/*
+	The purpose of this class is to manage the data to and from the server
+*/
 class liberator_client {
 
 	constructor(){
